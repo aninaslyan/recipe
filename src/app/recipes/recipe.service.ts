f239c88811e7cc582485e1233e2dfa669ac77fe7
aninaslyan <ani.naslyan@gmail.com>
2019-12-08 14:59:01 +0400

    http finished

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,29 +11,29 @@ export class RecipeService {
 
   constructor(private shoppingListService: ShoppingListService) {
   }
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tiramisu',
-      'is very tasty',
-      'https://www.maxpixel.net/static/photo/1x/Menu-Tasty-Cake-Sweet-Food-Tiramisu-Dessert-3338312.jpg',
-      [
-        new Ingredient('Cheese', 8),
-        new Ingredient('Cookie', 2),
-        new Ingredient('Milk', 1),
-        new Ingredient('Flour', 8)
-      ]
-    ),
-    new Recipe(
-      'Colorful',
-      'is not tasty',
-      'https://images-gmi-pmc.edge-generalmills.com/63b528dc-8beb-4289-96e3-6d8bef0a898d.jpg',
-      [
-        new Ingredient('Color', 3),
-        new Ingredient('Flour', 1)
-      ]
-    )
-  ];
+  private recipes: Recipe[] = [];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tiramisu',
+  //     'is very tasty',
+  //     'https://www.maxpixel.net/static/photo/1x/Menu-Tasty-Cake-Sweet-Food-Tiramisu-Dessert-3338312.jpg',
+  //     [
+  //       new Ingredient('Cheese', 8),
+  //       new Ingredient('Cookie', 2),
+  //       new Ingredient('Milk', 1),
+  //       new Ingredient('Flour', 8)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Colorful',
+  //     'is not tasty',
+  //     'https://images-gmi-pmc.edge-generalmills.com/63b528dc-8beb-4289-96e3-6d8bef0a898d.jpg',
+  //     [
+  //       new Ingredient('Color', 3),
+  //       new Ingredient('Flour', 1)
+  //     ]
+  //   )
+  // ];
 
   getRecipes() {
     return this.recipes.slice(); // copying
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { environment } from '../../environments/environment';
@@ -14,16 +15,33 @@ export class DataStorageService {
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    this.http.put(environment.apiUrl, recipes)
+    this.http
+      .put(
+        environment.apiUrl,
+        recipes
+      )
       .subscribe(response => {
         console.log(response);
       });
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>(environment.apiUrl)
-      .subscribe(response => {
-        this.recipeService.setRecipes(response);
-      });
+    return this.http
+      .get<Recipe[]>(
+        environment.apiUrl
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
